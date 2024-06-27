177b809690f8eaf6b8214df464443717af7c2a02
crampussm <swaparup36@gmail.com>
2024-06-28 02:44:21 +0530

    run command added

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
 
-const WS_URL = "ws://localhost:8080"
+// const WS_URL = "ws://localhost:8080"
+const WS_URL = "sketch-slam-backend"
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
